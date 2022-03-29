7193b8b8f2cd8f016f88d6cb84e2fbbc693fd4f1
Jeremy Varnham <jeremy.varnham@gmail.com>
2022-03-29 11:50:12 +0300

    Update formidable-custom-global-login-message.php
    Uses *string* in settings to indicate where link should be added

--- formidable-custom-global-login-message.php
+++ formidable-custom-global-login-message.php
@@ -3,7 +3,7 @@
 Plugin Name: Formidable Forms Login Message
 Plugin URI: https://github.com/jvarn/formidable-custom-global-login-message
 Description: Changes Global Login Message to include a login link
-Version: 1.0.0
+Version: 1.0.1
 Author: Jeremy Varnham
 Author URI: https://abuyasmeen.com/
 */
@@ -18,13 +18,23 @@ Author URI: https://abuyasmeen.com/
  */
 
 function jlv_ff_change_global_login_message( $message ) {
-	$current_url = home_url( add_query_arg( [], $GLOBALS['wp']->request ) ); // get_permalink() doesn't include query vars
-	$message = 'Please ';
-  $message .= '<a href="' . esc_url( wp_login_url( $current_url ) ) . '" alt="' . esc_attr__( 'Login', 'Formidable' ) . '">'; // login url uses current url to redirect back where they were after login
-  $message .= __( 'Login', 'Formidable' );
-	$message .= '</a>';
-	$message .= ' before filling this form.';
-  
-  return $message;
+	$frm_settings = FrmAppHelper::get_settings();
+	$frm_login_msg = $frm_settings->login_msg;
+	
+	if ( substr_count( $frm_login_msg, '*') == 2 ) {
+		$frm_login_msg_array = explode( "*", $frm_login_msg );
+		
+		$current_url = home_url( add_query_arg( [], $GLOBALS['wp']->request ) ); // get_permalink() doesn't include query vars
+		
+		$message = $frm_login_msg_array[0];
+	    $message .= '<a href="' . esc_url( wp_login_url( $current_url ) ) . '" alt="' . esc_attr__( 'Login', 'Wordpress' ) . '">';
+		$message .= $frm_login_msg_array[1];
+		$message .= '</a>';
+		$message .= $frm_login_msg_array[2];
+		
+	    return $message;
+    } else {
+	    return $frm_login_msg;
+    }
 }
 add_filter( 'frm_global_login_msg', 'jlv_ff_change_global_login_message' );
